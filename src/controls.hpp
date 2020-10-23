2903b2f8160ac947c1f3c575d436815ccbf636dc
Sridaran Thoniyil <sri7thon@gmail.com>
2020-10-23 17:54:52 -0400

    Create controls.hpp for button mappings

---
+++ src/controls.hpp
@@ -0,0 +1,13 @@
+#ifndef CONTROLS_H
+#define CONTROLS_H
+
+#include "main.h"
+
+namespace workshop {
+	namespace controls {
+		constexpr pros::controller_digital_e_t INTAKE_SPIN_OUT = pros::E_CONTROLLER_DIGITAL_R2;
+		constexpr pros::controller_digital_e_t INTAKE_SPIN_IN = pros::E_CONTROLLER_DIGITAL_R1;
+	}
+}
+
+#endif /* CONTROLS_H */
