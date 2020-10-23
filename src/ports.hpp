99f7cd999c3cd98538c4258a4661ee6af5b7ac65
Sridaran Thoniyil <sri7thon@gmail.com>
2020-10-23 10:05:46 -0400

    Make ports signed integers

--- src/ports.hpp
+++ src/ports.hpp
@@ -1,11 +1,11 @@
 #ifndef PORTS_H
 #define PORTS_H
 
-constexpr uint8_t PORT_DRIVE_LEFT_FRONT = 1;
-constexpr uint8_t PORT_DRIVE_RIGHT_FRONT = 2;
-constexpr uint8_t PORT_DRIVE_LEFT_BACK = 3;
-constexpr uint8_t PORT_DRIVE_RIGHT_BACK = 4;
 #include "stdint.h"
 
+constexpr int8_t PORT_DRIVE_LEFT_FRONT = 1;
+constexpr int8_t PORT_DRIVE_RIGHT_FRONT = 2;
+constexpr int8_t PORT_DRIVE_LEFT_BACK = 3;
+constexpr int8_t PORT_DRIVE_RIGHT_BACK = 4;
 
 #endif /* PORTS_H */
