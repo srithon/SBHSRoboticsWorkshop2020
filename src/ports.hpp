bf91024d593d082dc4a42fab512aedfac24fd781
Sridaran Thoniyil <sri7thon@gmail.com>
2020-10-23 18:54:41 -0400

    Define lift ports

--- src/ports.hpp
+++ src/ports.hpp
@@ -12,6 +12,9 @@ namespace workshop {
 	constexpr int8_t PORT_INTAKE_ROLLER_LEFT = 5;
 	constexpr int8_t PORT_INTAKE_ROLLER_RIGHT = 6;
 
+	constexpr int8_t PORT_LIFT_LEFT = 7;
+	constexpr int8_t PORT_LIFT_RIGHT = 8;
+
 	constexpr bool DRIVE_LEFT_FRONT_REVERSED = false;
 	constexpr bool DRIVE_RIGHT_FRONT_REVERSED = true;
 	constexpr bool DRIVE_LEFT_BACK_REVERSED = false;
@@ -19,6 +22,9 @@ namespace workshop {
 
 	constexpr bool INTAKE_ROLLER_LEFT_REVERSED = false;
 	constexpr bool INTAKE_ROLLER_RIGHT_REVERSED = true;
+
+	constexpr bool LIFT_LEFT_REVERSED = false;
+	constexpr bool LIFT_RIGHT_REVERSED = true;
 }
 
 #endif /* PORTS_H */
