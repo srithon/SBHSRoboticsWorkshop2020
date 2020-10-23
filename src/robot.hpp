1632267773d1b47ebe576d9031eec48010af8931
Sridaran Thoniyil <sri7thon@gmail.com>
2020-10-23 17:15:39 -0400

    Clarify lack of namespace prefixes for constants

--- src/robot.hpp
+++ src/robot.hpp
@@ -14,6 +14,8 @@ namespace workshop {
         Robot()
             // this->driveLeftFront = pros::Motor(PORT_DRIVE_LEFT_FRONT, DRIVE_LEFT_FRONT_REVERSED)
             // this is called a "member initializer list"
+            // NOTE -- reason we don't need to prefix constants with namespace
+            // is because we are in the context of the same namespace
             : driveLeftFront(PORT_DRIVE_LEFT_FRONT, DRIVE_LEFT_FRONT_REVERSED)
             , driveRightFront(PORT_DRIVE_RIGHT_FRONT, DRIVE_RIGHT_FRONT_REVERSED)
             , driveLeftBack(PORT_DRIVE_LEFT_BACK, DRIVE_LEFT_BACK_REVERSED)
