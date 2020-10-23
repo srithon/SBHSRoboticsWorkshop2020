ac87a9dc55373c4416c9c34f49a0b91a8eca28d9
Sridaran Thoniyil <sri7thon@gmail.com>
2020-10-23 18:55:06 -0400

    Define LIFT_UP and LIFT_DOWN controls

--- src/controls.hpp
+++ src/controls.hpp
@@ -7,6 +7,9 @@ namespace workshop {
 	namespace controls {
 		constexpr pros::controller_digital_e_t INTAKE_SPIN_OUT = pros::E_CONTROLLER_DIGITAL_R2;
 		constexpr pros::controller_digital_e_t INTAKE_SPIN_IN = pros::E_CONTROLLER_DIGITAL_R1;
+
+		constexpr pros::controller_digital_e_t LIFT_UP = pros::E_CONTROLLER_DIGITAL_L1;
+		constexpr pros::controller_digital_e_t LIFT_DOWN = pros::E_CONTROLLER_DIGITAL_L2;
 	}
 }
 
