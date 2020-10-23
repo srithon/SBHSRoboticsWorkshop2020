416f1c2ceb2f6ea3881aeb68602e0e6c82aff067
Sridaran Thoniyil <sri7thon@gmail.com>
2020-10-23 10:06:06 -0400

    Reverse right side motors

--- src/ports.hpp
+++ src/ports.hpp
@@ -4,8 +4,8 @@
 #include "stdint.h"
 
 constexpr int8_t PORT_DRIVE_LEFT_FRONT = 1;
-constexpr int8_t PORT_DRIVE_RIGHT_FRONT = 2;
+constexpr int8_t PORT_DRIVE_RIGHT_FRONT = -2;
 constexpr int8_t PORT_DRIVE_LEFT_BACK = 3;
-constexpr int8_t PORT_DRIVE_RIGHT_BACK = 4;
+constexpr int8_t PORT_DRIVE_RIGHT_BACK = -4;
 
 #endif /* PORTS_H */
